d1040f45f6029433299c10d8c284ebca055b95e9
Alex Granovsky <compfaculty@users.noreply.github.com>
2023-07-22 19:39:56 +0300

    prototype added

---
+++ MyInterview.Udemy/DesignPatternCourse/Creational/Prototype/Prototype.cs
@@ -0,0 +1,79 @@
+namespace MyIntervew.Udemy.DesignPatternCourse.Creational.Prototype;
+
+public class Prototype
+{
+    public void Test()
+    {
+        Person p1 = new Person();
+        p1.Age = 42;
+        p1.BirthDate = Convert.ToDateTime("1977-01-01");
+        p1.Name = "Jack Daniels";
+        p1.IdInfo = new IdInfo(666);
+
+        // Perform a shallow copy of p1 and assign it to p2.
+        Person p2 = p1.ShallowCopy();
+        // Make a deep copy of p1 and assign it to p3.
+        Person p3 = p1.DeepCopy();
+
+        // Display values of p1, p2 and p3.
+        Console.WriteLine("Original values of p1, p2, p3:");
+        Console.WriteLine("   p1 instance values: ");
+        DisplayValues(p1);
+        Console.WriteLine("   p2 instance values:");
+        DisplayValues(p2);
+        Console.WriteLine("   p3 instance values:");
+        DisplayValues(p3);
+
+        // Change the value of p1 properties and display the values of p1,
+        // p2 and p3.
+        p1.Age = 32;
+        p1.BirthDate = Convert.ToDateTime("1900-01-01");
+        p1.Name = "Frank";
+        p1.IdInfo.IdNumber = 7878;
+        Console.WriteLine("\nValues of p1, p2 and p3 after changes to p1:");
+        Console.WriteLine("   p1 instance values: ");
+        DisplayValues(p1);
+        Console.WriteLine("   p2 instance values (reference values have changed):");
+        DisplayValues(p2);
+        Console.WriteLine("   p3 instance values (everything was kept the same):");
+        DisplayValues(p3);
+    }
+
+    public static void DisplayValues(Person p)
+    {
+        Console.WriteLine(@"Name: , Age: {0:d}, BirthDate: {1:MM/dd/yy}", p.Age, p.BirthDate);
+        if (p.IdInfo != null) Console.WriteLine(@"ID#: {0:d}", p.IdInfo.IdNumber);
+    }
+}
+
+public class Person
+{
+    public int Age;
+    public DateTime BirthDate;
+    public string? Name;
+    public IdInfo? IdInfo;
+
+    public Person ShallowCopy()
+    {
+        return (Person)MemberwiseClone();
+    }
+
+    [Obsolete("Obsolete")]
+    public Person DeepCopy()
+    {
+        Person clone = (Person)MemberwiseClone();
+        if (IdInfo != null) clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+        if (Name != null) clone.Name = String.Copy(Name);
+        return clone;
+    }
+}
+
+public class IdInfo
+{
+    public int IdNumber;
+
+    public IdInfo(int idNumber)
+    {
+        IdNumber = idNumber;
+    }
+}
